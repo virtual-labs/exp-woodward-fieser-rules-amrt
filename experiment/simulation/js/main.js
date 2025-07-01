75fb25720aac64f38e5c07280b40efbd35532bef
Saneesh P F <41299081+saneeshpfrancis@users.noreply.github.com>
2025-07-01 17:58:35 +0530

    Update main.js

--- experiment/simulation/js/main.js
+++ experiment/simulation/js/main.js
@@ -1,4 +1,11 @@
-//Your JavaScript goes in here
+// Get the base path dynamically
+const currentUrl = window.location.href;
+const basePath = currentUrl.substring(0, currentUrl.lastIndexOf("/"));
+const swfPath = basePath + "/flash/simulation/index.swf";
+
+// Set the dynamic link text
+document.getElementById("copyText").textContent = swfPath;
+
 function copyLink() {
   const text = document.getElementById("copyText").textContent;
   navigator.clipboard.writeText(text).then(() => {
